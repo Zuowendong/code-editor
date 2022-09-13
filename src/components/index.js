6853b0020e2271ff644a17b2599c6ad156a62de4
zuowendong <17326195691@163.com>
2022-09-13 14:43:24 +0800

    feat: init

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ZyfContainer } from "./ZyfContainer/index.vue";
-export { default as ZyfText } from "./ZyfText/index.vue";
-export { default as ZyfInput } from "./ZyfInput/index.vue";
-
-
--- src/utils/compTemplateData.js
+++
@@ -1,7 +0,0 @@
-// 组件数据标准model
-export const compTemplateData = (...args) => {
-    return {
-        ...args[0],
-        uuid: `${args[0].type}_${Date.now()}`,
-    };
-};
